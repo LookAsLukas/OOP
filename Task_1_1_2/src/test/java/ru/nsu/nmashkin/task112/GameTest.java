687116affdf4bb763454f4805ecc16ee00115d79
LookAsLukas <kolyabetmen231006@gmail.com>
2025-09-07 23:26:17 +0700

    test enrichment

--- Task_1_1_2/src/test/java/ru/nsu/nmashkin/task112/GameTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/nmashkin/task112/GameTest.java
@@ -10,7 +10,7 @@ class GameTest {
     @Test
     void main() {
         InputStream in = System.in;
-        System.setIn(new ByteArrayInputStream("1\n0\n1\n0\n0\n0\n".getBytes()));
+        System.setIn(new ByteArrayInputStream("lol\n0\nlol\n1\n0\n1\n0\n0\n".getBytes()));
         Game dummy = new Game();
         dummy.main(null);
         System.setIn(in);
@@ -20,13 +20,16 @@ class GameTest {
     @Test
     void round() {
         InputStream in = System.in;
-        System.setIn(new ByteArrayInputStream("1\n0\n0\n0\n".getBytes()));
+        System.setIn(new ByteArrayInputStream("lol\n1\n0\n0\n0\n".getBytes()));
         Game dummy = new Game();
-        try {
-            dummy.round();
-        } catch (IllegalStateException e) {
-            System.out.println("No idea what went wrong");
-        }
+        dummy.round();
+        System.setIn(in);
+        dummy.destroyScanner();
+
+        in = System.in;
+        System.setIn(new ByteArrayInputStream("1\n1\n1\n0\n0\n0\n".getBytes()));
+        dummy = new Game();
+        dummy.round();
         System.setIn(in);
         dummy.destroyScanner();
     }
