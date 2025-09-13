0a884c8c478c6448691d66b3a8847129f52616c9
LookAsLukas <kolyabetmen231006@gmail.com>
2025-09-13 21:16:49 +0700

    forgor javadoc

--- Task_1_1_3/src/main/java/ru/nsu/nmashkin/task113/ExpressionException.java
+++ Task_1_1_3/src/main/java/ru/nsu/nmashkin/task113/ExpressionException.java
@@ -1,6 +1,11 @@
 package ru.nsu.nmashkin.task113;
 
 public class ExpressionException extends RuntimeException {
+    /**
+     * Custom exception class for Expression.
+     *
+     * @param message exception message
+     */
     public ExpressionException(String message) {
         super(message);
     }
--- Task_1_1_3/src/main/java/ru/nsu/nmashkin/task113/ParserException.java
+++ Task_1_1_3/src/main/java/ru/nsu/nmashkin/task113/ParserException.java
@@ -1,6 +1,11 @@
 package ru.nsu.nmashkin.task113;
 
 public class ParserException extends RuntimeException {
+    /**
+     * Custom exception class for Parser.
+     *
+     * @param message exception message
+     */
     public ParserException(String message) {
         super(message);
     }
