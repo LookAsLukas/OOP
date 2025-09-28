9c527e41e3cbe570b85375cd93d6fa07b9833a39
LookAsLukas <kolyabetmen231006@gmail.com>
2025-09-28 22:18:26 +0700

    initiall commit

---
+++ Task_1_2_2/src/main/java/ru/nsu/nmashkin/task122/HashTableException.java
@@ -0,0 +1,15 @@
+package ru.nsu.nmashkin.task122;
+
+/**
+ * Exception class for HashTable.
+ */
+public class HashTableException extends RuntimeException {
+    /**
+     * Initialize an exception.
+     *
+     * @param message exception message
+     */
+    public HashTableException(String message) {
+        super(message);
+    }
+}
