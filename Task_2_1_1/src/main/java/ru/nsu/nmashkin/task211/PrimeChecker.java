a466b6fbf68736fb15f35c1c6e71614a8c38fa54
LookAsLukas <kolyabetmen231006@gmail.com>
2026-02-13 22:32:10 +0700

    at last, we meet again, reviewdog...

---
+++ Task_2_1_1/src/main/java/ru/nsu/nmashkin/task211/PrimeChecker.java
@@ -0,0 +1,19 @@
+package ru.nsu.nmashkin.task211;
+
+public class PrimeChecker {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Task_2_1_1/src/test/java/ru/nsu/nmashkin/task211/TestBenchmark.java
@@ -0,0 +1,50 @@
+package ru.nsu.nmashkin.task211;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class TestBenchmark {
+    private int[] generatePrimes() {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 6700000; i < 6800000; i++) {
+            if (PrimeChecker.isPrime(i)){
+                result.add(i);
+            }
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+
+    @Test
+    void benchmark() {
+        int[] nums = generatePrimes();
+
+        long start = System.nanoTime();
+        assertFalse(Sequential.hasNonPrime(nums));
+        long timeSequential = System.nanoTime() - start;
+
+        final int numThreads = 10;
+        long[] timeMultiThreaded = new long[numThreads];
+        for (int threadCnt = 1; threadCnt <= numThreads; threadCnt++) {
+            start = System.nanoTime();
+            assertFalse(MultiThreaded.hasNonPrime(nums, threadCnt));
+            timeMultiThreaded[threadCnt - 1] = System.nanoTime() - start;
+        }
+
+        start = System.nanoTime();
+        assertFalse(ParallelStream.hasNonPrime(nums));
+        long timeParallelStream = System.nanoTime() - start;
+
+        double divisor = 1000000;
+        System.out.println("Sequential: " + (double) timeSequential / divisor);
+        System.out.println("MultiThreaded:");
+        for (int threadCnt = 1; threadCnt <= numThreads; threadCnt++) {
+            System.out.println("  " + threadCnt + " --> " + (double) timeMultiThreaded[threadCnt - 1] / divisor);
+        }
+        System.out.println("ParallelStream: " + (double) timeParallelStream / divisor);
+    }
+}
